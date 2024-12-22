6676ae655fbf3ada83aca5cb875f0594c970eacd
Trungbeso <trungnq555@gmail.com>
2024-12-22 09:29:23 +0700

    implement Category Controller

--- src/main/java/com/trungbeso/dreamshops/response/ApiResponse.java
+++ src/main/java/com/trungbeso/dreamshops/response/ApiResponse.java
@@ -2,9 +2,11 @@ package com.trungbeso.dreamshops.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse {
 	private String message;
 	private Object data;
